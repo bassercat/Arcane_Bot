0b3575c7524aa71647d9f09e2a6baecb6a5ad943
PeiXing <bassercat@yahoo.com.tw>
2025-06-18 17:07:44 +0800

    Update vc_join_leave.py

--- vc_join_leave.py
+++ vc_join_leave.py
@@ -11,7 +11,6 @@ class vc_join_leave_prefix(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
 
-  # j1 指令：加入語音頻道
   @commands.command(name="jvc")
   async def join_vc(self, ctx, *, channel_name_or_id: str):
 
@@ -24,18 +23,15 @@ class vc_join_leave_prefix(commands.Cog):
     if ctx.guild.voice_client:
       await ctx.guild.voice_client.disconnect(force=True)
 
-    # 嘗試用 ID 找頻道
     try:
       channel_id = int(channel_name_or_id)
       target_channel = discord.utils.get(ctx.guild.voice_channels, id=channel_id)
     except ValueError:
-      # 否則用名稱找
       target_channel = discord.utils.get(ctx.guild.voice_channels, name=channel_name_or_id)
 
     if not target_channel:
       return
 
-    # 已在語音頻道就不要重複加入
     if ctx.voice_client:
       await ctx.voice_client.move_to(target_channel)
       await ctx.send(f"Joined {target_channel.name}")
@@ -43,7 +39,6 @@ class vc_join_leave_prefix(commands.Cog):
       await target_channel.connect()
       await ctx.send(f"Joined {target_channel.name}")
 
-  # j2 指令：離開語音頻道
   @commands.command(name="lvc")
   async def leave_vc(self, ctx):
 
@@ -53,7 +48,6 @@ class vc_join_leave_prefix(commands.Cog):
     if ctx.channel.id != VC_JOIN_LEAVE["command"]:
       return
 
-    # 讓 bot 離開語音頻道
     if ctx.voice_client:
       channel = ctx.voice_client.channel
       await ctx.voice_client.disconnect()
@@ -61,6 +55,5 @@ class vc_join_leave_prefix(commands.Cog):
     else:
       await ctx.send("Not in voice channel")
 
-#COG 加入
 async def setup(bot):
   await bot.add_cog(vc_join_leave_prefix(bot))
