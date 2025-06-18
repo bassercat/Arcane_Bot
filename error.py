d683e779802055aa572e5e1d74dee64397211275
PeiXing <bassercat@yahoo.com.tw>
2025-06-18 17:03:18 +0800

    Update error.py

--- error.py
+++ error.py
@@ -1,17 +1,16 @@
 from discord.ext import commands
 
-# 靜默處理沒有權限 冷卻中 錯誤
 
 async def error(ctx, error):
   if isinstance(error, commands.CheckFailure):
-    # 沒權限就不回應任何訊息
+
     return
   elif isinstance(error, commands.CommandOnCooldown):
-    # 冷卻中也不回應任何訊息
+
     return
   elif isinstance(error, commands.CommandNotFound):
-    # 靜默忽略指令不存在的錯誤，不輸出任何訊息
+
     return
   else:
-    # 其餘錯誤仍拋出（方便除錯）
+
     raise error
