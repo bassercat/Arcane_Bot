0e17219fdb19e4fe1e8cefab1d69b67879b5b146
PeiXing <bassercat@yahoo.com.tw>
2025-06-18 17:03:05 +0800

    Update emoji_utils.py

--- emoji_utils.py
+++ emoji_utils.py
@@ -1,22 +1,16 @@
-# 輸入:自訂: 轉換成<:emoji_name:emoji_id> Discord 自訂 emoji 語法（字串）
-# 字串轉換，讓純文字訊息能包含表情
-#使用:
-#content = emoji_utils(content, channel.guild)
 
 def emoji_text(content, guild):
     if not guild:
         return content
     for emoji in guild.emojis:
         tag = f":{emoji.name}:"
-        # 會變成 <:{name}:{id}>
+
         code = str(emoji)  
         content = content.replace(tag, code)
     return content
 
 
-#從 guild.emojis 產生名稱對應 emoji 的字典
-#emoji_map = get_emoji_map(ctx.guild)
-#emoji_map.get(name, name)
+
 def get_emoji_map(guild):
   
   return {emoji.name: emoji for emoji in guild.emojis}
