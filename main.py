5b95106622f02009040e05cc293c9805de7ec5cd
PeiXing <bassercat@yahoo.com.tw>
2025-06-18 16:49:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-# 判斷執行環境
 from config.env_setup import setup_env
 ENV = setup_env()
 
-## 匯入模組
-# 標準函式庫
 import asyncio
 import os
 import json
 import os
 
-# 第三方套件
 import discord
 from discord.ext import commands
 from discord.ext.commands import CommandOnCooldown
@@ -17,9 +13,7 @@ from dotenv import load_dotenv
 from discord import app_commands
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-# 載入 .env 的環境變數
 load_dotenv(dotenv_path="config/token.env")
-# 從環境變數中取得 TOKEN
 if ENV == 'local':
   TOKEN = os.getenv("TOKEN_1")
 elif ENV == 'colab':
@@ -29,22 +23,15 @@ else:
 
 PREFIX = os.getenv("PREFIX", "!")
 
-## 個人函式庫
-# 讀取json
 from utils.load_json import load_json, json_data
-# 靜默處理沒有權限 冷卻中 錯誤
 from utils.error import error
-# 支援用頻道 ID（int 或字串）或頻道名稱（字串）來取得頻道物件
 from utils.channel_utils import get_channel
 
 
-# 建立 Intents 物件
 from config.settings import Intents
 
-# 建立機器人
 bot = commands.Bot(command_prefix=PREFIX, intents=Intents)
 
-# 靜默處理沒有權限 冷卻中 錯誤
 bot.on_command_error = error
 
 @bot.event
@@ -53,24 +40,23 @@ async def on_message(message):
     return
   await bot.process_commands(message)
 
-# 載入事件
 @bot.event
 async def on_ready():
   MAIN = json_data['MAIN']
 
-  # 預載圖片並快取 因json讀取放這讀
+
   from utils.card_image_utils import cache_card_images
   cache_card_images()
 
   print(f"Synced {len(bot.commands)} prefix commands")
 
   if MAIN["remove_global_commands"]:
-    #清空全域指令（移除 + 空同步）
+
     global_commands = await bot.tree.fetch_commands()
     for cmd in global_commands:
       bot.tree.remove_command(cmd.name)
     print(f"Removed {len(global_commands)} global slash & context menu commands")
-    # 將空指令同步回 Discord，真正刪除全域指令
+
     await bot.wait_until_ready()
     #await asyncio.sleep(1)
     await bot.tree.sync()
@@ -118,19 +104,17 @@ async def on_ready():
   print(f'Logged in as {bot.user}')
   print(f'------------------------------')
 
-# 啟動機器人
+
 async def main():
   async with bot:
     load_json(ENV)
 
-    # 建立並啟動APScheduler 排程器（只需要在 bot 啟動時建立一次）
-    # 把 scheduler 綁到 bot 屬性，方便 Cog 取得
+
     bot.scheduler = AsyncIOScheduler()
     bot.scheduler.start()
 
 
 
-    ## cogs
 
     # prefix 指令 !s say_command
     await bot.load_extension("cogs.commands.prefix.say_command")
@@ -155,7 +139,7 @@ async def main():
 
 
 
-    """
+
     # slash 指令 /s say_command
     await bot.load_extension("cogs.commands.slash.say_command")
     # slash 指令 /e emoji_command
@@ -176,7 +160,7 @@ async def main():
     await bot.load_extension("cogs.commands.slash.get_gacha_data")
     # slash 指令 /vcs /vcss vc_speak
     await bot.load_extension("cogs.commands.slash.vc_speak")
-    """
+
 
 
 
@@ -188,7 +172,6 @@ async def main():
     # 聊天室自動表情回應 auto emoji react
     # 指令轉換 command_conversion
     # 聊天室抽一個 chat_draw_one
-    # 統一放on_message
     await bot.load_extension("cogs.listeners.on_message")
 
     # 附和反應 follow_reaction
