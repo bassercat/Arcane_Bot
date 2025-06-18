ddfd6f4a5713dd85caf8caf2a6f9d536d4ac0631
PeiXing <bassercat@yahoo.com.tw>
2025-06-18 16:57:57 +0800

    Update bot_status.py

--- bot_status.py
+++ bot_status.py
@@ -30,14 +30,12 @@ class bot_status(commands.Cog):
     now_set = BOT_STATUS[set_]
     guild = self.bot.get_guild(MAIN['guild_id'])
 
-    # 1.嘗試上傳頭像（若未設定過）
     with open(now_set['avatar'], "rb") as f:
       avatar_bytes = f.read()
     target_hash = hash_bytes(avatar_bytes)
 
     current_hash = None
 
-    # 有頭像才讀取
     if self.bot.user.avatar:  
       current_bytes = await self.bot.user.avatar.read()
       current_hash = hash_bytes(current_bytes)
@@ -52,7 +50,6 @@ class bot_status(commands.Cog):
 
 
 
-    # 2.修改在每個伺服器的暱稱
     try:
       if guild is not None:
         me = guild.me
@@ -65,25 +62,16 @@ class bot_status(commands.Cog):
 
 
 
-    # 3.設定 BOT 活動狀態
     activity = discord.CustomActivity(name=now_set["activity"])
 
-    #discord.Game(name="")        # 顯示為：正在遊玩
-    #discord.Streaming(name="", url="")  # 顯示為：正在直播
-    #discord.Activity(name="", type=discord.ActivityType.watching)  # 顯示為：正在觀看
-    #discord.Activity(name="", type=discord.ActivityType.listening) # 顯示為：正在聆聽
-    #discord.Activity(name="", type=discord.ActivityType.competing) # 顯示為：正在參加
+
 
     status = now_set.get("status", "online")
     # 字串合併屬性
     status_enum = getattr(discord.Status, status)
     await self.bot.change_presence(status=status_enum, activity=activity)
     
-    #dscord.Status.online	線上
-    #discord.Status.idle	閒置
-    #discord.Status.dnd	請勿打擾
-    #discord.Status.invisible	隱形
-    #discord.Status.offline	離線
+
 
 async def setup(bot: commands.Bot):
   await bot.add_cog(bot_status(bot))
